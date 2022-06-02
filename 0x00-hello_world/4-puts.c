3f4570e5910d3bd59498f99803c030f5ea8ddffe
vivian347 <vkinyua001@gmail.com>
2022-06-02 11:26:04 -0700

    correction on betty commenting

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-/**
- *  * main - prints using put
- *   *
- *    * Return: Always 0 (Success)
- *     */
+/* main - prints using put
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
 	    return (0);
 }
