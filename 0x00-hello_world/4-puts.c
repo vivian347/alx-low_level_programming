4d4677730a2175029994080e3dcce07a03c9ae3a
vivian347 <vkinyua001@gmail.com>
2022-06-02 11:30:49 -0700

    correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-/* main - prints using put
+/**
+ * main - prints using put
+ *
  *
  * Return: Always 0 (Success)
  */
@@ -8,5 +10,5 @@ int main(void)
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
-	    return (0);
+      	return (0);
 }
