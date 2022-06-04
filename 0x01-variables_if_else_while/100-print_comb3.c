121e3b78db7d8b863f08d692ec4ca2fd173f39aa
vivian347 <vkinyua001@gmail.com>
2022-06-04 08:15:42 -0700

    combinationof 2

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,16 +10,16 @@ int main(void)
 {
 	int digit1, digit2;
 
-	for (digit1 = 0; digit1 < 10; digit1++)
+	for (digit1 = 0; digit1 < 8; digit1++)
 	{
-		for (digit2 = 0; digit2 < 10; digit2++)
+		for (digit2 = 1; digit2 < 10; digit2++)
 		{
 																				if ((digit1 <= 8 && digit2 <= 9) || (digit1 != digit2) || (digit1 != 1 && digit2 != 0))
 			{
 				putchar((digit1 % 10) + '0');
 				putchar((digit2 % 10) + '0');
 
-				continue;
+				break;
 			}														
 																				putchar(',');
 																				putchar(' ');
