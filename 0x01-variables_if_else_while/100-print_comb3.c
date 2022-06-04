6ecb96b25a9d7043432ffe0dcc11259a0e610dbe
vivian347 <vkinyua001@gmail.com>
2022-06-04 07:26:49 -0700

    combinationof 2

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,8 +18,14 @@ int main(void)
 			putchar((dig1 % 10) + '0');
 			putchar((dig2 % 10) + '0');
 
-			if ((dig1 == 9) && (dig2 == 9))
+			if ((dig1 == 8) && (dig2 == 9))
+			{
 				continue;
+			}
+			else if ((dig1 != dig2) || ((dig1 != 1) && (dig2 != 0))
+					{
+					continue;
+					}
 
 			putchar(',');
 			putchar(' ');
