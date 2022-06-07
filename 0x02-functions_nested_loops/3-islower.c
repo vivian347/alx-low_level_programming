db0875c40fd78367d9320918d78e7a76d0086622
vivian347 <vkinyua001@gmail.com>
2022-06-07 12:48:09 -0700

    return 1 if lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - returns 1 if letter is lowercase
+ *
+ * @c - character to be checked
+ *
+ * Return - 1 if lower, 0 otherwise.
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+
+}
