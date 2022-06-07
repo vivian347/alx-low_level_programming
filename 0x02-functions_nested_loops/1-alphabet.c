1d3839634b319c14c1d133b15ae46adb44379aa7
vivian347 <vkinyua001@gmail.com>
2022-06-07 08:08:14 -0700

    lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet inlowercase.
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a' ; letter <= 'z' ; letter++)
+	{
+		_putchar(letter);
+	}
+
+	_putchar('\n');
+}
