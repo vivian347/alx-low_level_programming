e96b939b2dfca0e88aaf68f344b1f80dff5309e7
vivian347 <vkinyua001@gmail.com>
2022-06-08 09:32:24 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_to_98 - naturalnumbers to 98.
+ * @n: integer to be computed.
+ */
+
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+		{
+			_putchar((n % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
+
+			n--;
+		}
+
+		_putchar((n % 10) + '0');
+	}
+	else
+	{
+		while (n < 98)
+		{
+			_putchar((n % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
+
+			n++;
+		}
+
+		_putchar((n % 10) + '0');
+	}
+}
+
