759739cbd8c73d7330d7ec41bcd2805b663d1cc3
vivian347 <vkinyua001@gmail.com>
2022-06-08 09:37:26 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,11 +12,11 @@ void print_to_98(int n)
 		while (n > 98)
 		{
 			_putchar((n % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
-
-			n--;
+			
+			n -= 1
 		}
+		_putchar(',');                                                                                                  
+		_putchar(' ');
 
 		_putchar((n % 10) + '0');
 	}
