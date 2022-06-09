9e7f51714b263066371e0c2fff4c8b1d9ffc6be1
vivian347 <vkinyua001@gmail.com>
2022-06-09 09:15:44 -0700

    10x

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,25 +1,21 @@
 #include "main.h"
 
 /**
- * more_numbers - numbers 0-14 10x.
+ * more_numbers - 0 to 14 10x.
  */
 
 void more_numbers(void)
 {
-	int count = 0;
-	int num;
+	int count, num;
 
-	while (count <= 9)
+	for (count = 0 ; count < 10 ; count++)
 	{
 		for (num = 0 ; num <= 14 ; num++)
 		{
 			if (num > 9)
-			{
-				_putchar((num / 10) + 48);
-				_putchar((num % 10) + 48);
-			}
+				_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
 		}
 		_putchar('\n');
-		count++;
 	}
 }
