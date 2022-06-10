8681b3daa2ba97055b4313793b19934b19a02088
vivian347 <vkinyua001@gmail.com>
2022-06-09 23:51:43 -0700

    diag

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,14 +8,13 @@
 void print_diagonal(int n)
 {
 	int size, i;
-	char b_slash = '\';
 
 	for (i = 0 ; i < n ; i++)
 	{
 		for (size = 0 ; size < n ; size++)
 		{
 			if (size == i)
-				_putchar(b_slash);
+				_putchar('\\');
 			else
 				_putchar(' ');
 		}
