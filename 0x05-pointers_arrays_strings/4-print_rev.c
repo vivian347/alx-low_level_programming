363ce77078648c274fe4a7f5ed5969d8076b96a3
vivian347 <vkinyua001@gmail.com>
2022-06-13 14:13:28 -0700

    3,4,5

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,12 +7,12 @@
 
 void print_rev(char *s)
 {
-	int len = 0, i;
+	int len = 0, index;
 
-	while (s[i++])
+	while (s[index++])
 		len++;
 
-	for (i = len - 1 ; i >= 0 ; i--)
-		_putchar(s[i]);
+	for (index = len - 1 ; index >= 0 ; index--)
+		_putchar(s[index]);
 	_putchar('\n');
 }
