07ffcdb80e5509a1215f8ead14e30fae46a8a061
vivian347 <vkinyua001@gmail.com>
2022-06-14 06:07:11 -0700

    every other

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character.
+ * @str: string to be computed
+ */
+
+void puts2(char *str)
+{
+	int index = 0, len = 0;
+
+	while (str[index++])
+		len++;
+	for (index = 0 ; index < len ; index += 2)
+	{
+		_putchar(str[index]);
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,5 +5,9 @@ int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void puts2(char *str);
+int _puts(char *str);
+void rev_string(char *s);
+void print_rev(char *s);
 
 #endif
