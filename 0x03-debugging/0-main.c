625e2342269ba58cf7456169dde2b772761366db
vivian347 <vkinyua001@gmail.com>
2022-06-25 04:45:09 -0700

    test0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests fnthat prints whether integer is poitive or not
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positi_or_negative(i);
+	return (0);
+}
