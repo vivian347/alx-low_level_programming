76bcec735988b26d80557604ca6ddb64d817441c
vivian347 <vkinyua001@gmail.com>
2022-07-18 08:40:34 -0700

    others

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free-list - frees list
+ * @head: start of list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
