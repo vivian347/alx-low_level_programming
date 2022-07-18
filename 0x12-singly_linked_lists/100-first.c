6002b23ba31265c083540600a897e3c4a9be0064
vivian347 <vkinyua001@gmail.com>
2022-07-18 13:38:42 -0700

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+void __attribute__((constructor)) func(void);
+
+/**
+ * func - isprinted before mainis executed
+ */
+
+void __attribute__((constructor)) func(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
