437da01759becd2a4f28fabc7bc08963d761528f
vivian347 <vkinyua001@gmail.com>
2022-07-23 11:18:28 -0700

    10

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -15,7 +15,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 	mask = (1 << index);
-	if (mask | num)
+	if (mask ^ num)
 		return (1);
 	return (1);
 }
